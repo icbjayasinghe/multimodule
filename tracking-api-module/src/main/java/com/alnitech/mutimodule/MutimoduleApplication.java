8463d728b728f11197c795f7e6e8f6d91b548b7b
Isuru <icbjayasinghe@gmail.com>
2021-09-30 08:24:24 +0530

    Initial commit

---
+++ tracking-api-module/src/main/java/com/alnitech/mutimodule/MutimoduleApplication.java
@@ -0,0 +1,13 @@
+package com.alnitech.mutimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutimoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutimoduleApplication.class, args);
+    }
+
+}
